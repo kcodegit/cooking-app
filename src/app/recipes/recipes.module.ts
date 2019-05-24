ffc5b56c5383b5d95dcf3409fd18b68f65498bdb
kohei <andokohei@gmail.com>
2019-05-24 18:19:27 +0900

    Modules refactoring

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RecipesComponent } from './recipes.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { DropdownDirective } from '../shared/dropdown.directive';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeEditComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    RecipeStartComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, RecipesRoutingModule, SharedModule],
+})
+export class RecipesModule {}
